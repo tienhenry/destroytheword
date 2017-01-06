2661b39ac483a1d6dfd9f5a0d0fe637d3d7efdee
NGUYEN BA TIEN <tien2b@gmail.com>
2017-01-06 09:45:43 +0700

    update

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by NGUYEN BA TIEN on 1/4/17.
+//  Copyright © 2017 tienhenry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
